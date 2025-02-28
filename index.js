a488048699bd9016c887d1007597c53f080d17e5
Lucas-Slr <lucas.soulairol@gmail.com>
2025-02-28 17:47:42 +0100

    fix

--- index.js
+++ index.js
@@ -33,12 +33,13 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connecté');
-        createAdminUser();
     })
     .catch(err => console.error('Erreur de connexion MongoDB:', err));
 
-// Routes
+    // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api/users', userRoutes);
+
 
 // const createAdminUser = async () => {
 //     try {
